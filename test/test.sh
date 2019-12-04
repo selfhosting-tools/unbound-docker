a33603cd773f23633a74e9d10667aec43620715c
FL42 <46161216+fl42@users.noreply.github.com>
2019-12-04 23:06:23 +0100

    feat: improve tests

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+set -o pipefail
 
 CONTAINER_IP=$(docker inspect --format '{{.NetworkSettings.IPAddress}}' unbound)
 
-dig google.fr @"$CONTAINER_IP"
+dig google.fr @"$CONTAINER_IP" | grep NOERROR
+
+dig dnssec-tools.org @"$CONTAINER_IP" | grep NOERROR
+dig dnssec-tools.org @"$CONTAINER_IP" | grep ".*flags:.*ad.*"
+
+dig dnssec-failed.org @"$CONTAINER_IP" | grep SERVFAIL
